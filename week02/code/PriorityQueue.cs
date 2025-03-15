c8a6cdc9601cdfd1c89191b8dbaade4e6cbc697f
EduardOrellana <ore23006@byui.edu>
2025-03-15 11:07:09 -0600

    problem solving 2

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -2,8 +2,10 @@
 {
     private List<PriorityItem> _queue = new();
 
+    public int Length => _queue.Count;
+
     /// <summary>
-    /// Add a new value to the queue with an associated priority.  The
+    /// Add a new value to the queue with an associated priority. The
     /// node is always added to the back of the queue regardless of 
     /// the priority.
     /// </summary>
@@ -24,14 +26,15 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)  // Fix: change _queue.Count - 1 to _queue.Count
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);  // Remove the item from the queue
         return value;
     }
 
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,28 +1,69 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// TODO Problem 2 - Write and run test cases and fix the code to match requirements.
-
 [TestClass]
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Create a queue with the following items and priorities: Erick (2), Eduardo (5), Cesar (3) and
+    // run until the queue is empty
+    // Expected Result: Eduardo, Cesar, Erick
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var erick = new PriorityItem("Erick", 2);
+        var eduardo = new PriorityItem("Eduardo", 5);
+        var cesar = new PriorityItem("Cesar", 3);
+
+        PriorityItem[] expectedResult = { eduardo, cesar, erick };
+
+        var players = new PriorityQueue();
+        players.Enqueue(erick.Value, erick.Priority);
+        players.Enqueue(eduardo.Value, eduardo.Priority);
+        players.Enqueue(cesar.Value, cesar.Priority);
+
+        int i = 0;
+        while (players.Length > 0)
+        {
+            if (i >= expectedResult.Length)
+            {
+                Assert.Fail("Queue should have run out of items by now.");
+            }
+
+            var person = players.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, person);
+            i++;
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Create a queue with the following items and priorities: Erick (15), Eduardo (5), Cesar (5)
+    // Expected Result: Eduardo, Cesar, Erick
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var erick = new PriorityItem("Erick", 15);
+        var eduardo = new PriorityItem("Eduardo", 5);
+        var cesar = new PriorityItem("Cesar", 5);
+
+        PriorityItem[] expectedResult = { erick, eduardo, cesar };
+
+        var players = new PriorityQueue();
+        players.Enqueue(erick.Value, erick.Priority);
+        players.Enqueue(eduardo.Value, eduardo.Priority);
+        players.Enqueue(cesar.Value, cesar.Priority);
+
+        int i = 0;
+        while (players.Length > 0)
+        {
+            if (i >= expectedResult.Length)
+            {
+                Assert.Fail("Queue should have run out of items by now.");
+            }
+
+            var person = players.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, person);
+            i++;
+        }
     }
 
     // Add more test cases as needed below.
